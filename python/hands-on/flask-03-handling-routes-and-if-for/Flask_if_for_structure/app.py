ed4b27be3e89f379d1a8a66cfe0b9414ca8fad68
osmancibik <osmancibikli@gmail.com>
2021-11-30 23:12:35 +0300

    flask-03-if-for added

--- python/hands-on/flask-03-handling-routes-and-if-for/Flask_if_for_structure/app.py
+++ python/hands-on/flask-03-handling-routes-and-if-for/Flask_if_for_structure/app.py
@@ -1,19 +1,21 @@
 from flask import Flask, render_template
 
-app = Flask (__name__)
+app = Flask(__name__)
 
 @app.route('/')
 def head():
-    first="This is my first condition experience"
-    return render_template("index.html",  message = False )
+    first = 'This is my first condition experience'
+    return render_template('index.html', message = first)
 
 @app.route('/osman')
 def mylist():
-    names = ["Ahmet Arif", "Salih", "Tarkan", "Tanju", "Halil"]
+    names = ["Osman", "Sergen", "Halil", "Ahmet Arif"]
     
-    return render_template('body.html', object=names)
+    return render_template('body.html', object = names )
 
 
-if __name__ == "__main__":
-    app.run(debug=True)
+
+
+if __name__ == '__main__':
+    #app.run(debug=True)
     app.run(host='0.0.0.0', port=80)
